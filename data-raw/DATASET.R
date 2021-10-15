915d8f1c852a6be103b95c751d840ee9f6725b6d
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-10-15 17:41:41 +0200

    palettes

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
+# ---------- Create vector of colors ----------
 epi_cols <- tibble::tibble(
   name = c(
     "Epinion Red",
@@ -32,11 +33,56 @@ epi_cols <- tibble::tibble(
 epi_cols <- tibble::deframe(epi_cols)
 
 
-# ----- Upload data to GitHub
+# Upload data to GitHub
 usethis::use_data(epi_cols,
                   overwrite = TRUE)
 
 # https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
 
+
+
+# ---------- Create list with continuous color palettes ----------
+epipal_c <- list(
+
+  `epiblue` = epi_colvec("Epi_WarmSand", "Epi_ClearBlue", "Epi_DarkBlue"),
+
+  `epipurple` = epi_colvec("Epi_WarmSand", "Epi_LightPurple", "Epi_DarkPurple"),
+
+  `epigreen` = epi_colvec("Epi_WarmSand", "Epi_LightGreen", "Epi_Green"),
+
+  `epigold` = epi_colvec("Epi_WarmSand", "Epi_LightGold", "Epi_Gold")
+
+)
+
+
+# Upload data to GitHub
+usethis::use_data(epipal_c,
+                  overwrite = TRUE)
+
+
+
+# ---------- Create list with discrete color palettes ----------
+epipal_d <- list()
+
+epipal_d$main <- list(
+
+  "Epi_DarkBlue" = "#0F283C",
+  "Epi_WarmSand" = "#E8E1D5",
+  "Epi_DarkPurple" = "#641E3C",
+  "Epi_ClearBlue" = "#233CA0",
+  "Epi_LightDeepBlue" = "#68838B",
+  "Epi_LightPurple" = "#BA7384",
+  "Epi_LightBlue" = "#A7C7D7",
+  "Epi_Green" = "#004337",
+  "Epi_LightGreen" = "#73A89A",
+  "Epi_Gold" = "#C18022",
+  "Epi_LightGold" = "#EBC882",
+  "Epi_Red" = "#E13C32"
+
+)
+
+# Upload data to GitHub
+usethis::use_data(epipal_d,
+                  overwrite = TRUE)
 
 
