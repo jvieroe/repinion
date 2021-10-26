7a643d9dc85a1f7083f8f2be0d7581d863ad614b
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-10-26 13:19:19 +0200

    update epi themes

--- R/epitheme_dust.R
+++ R/epitheme_dust.R
@@ -2,7 +2,8 @@
 #'
 #' This function applies a theme consistent with the Epinion Corporate Visual Identity (CVI) to your `ggplot2` graphics. Add it to your `ggplot2` syntax for conistent visual outputs.
 #' @param legend a logical value indicating whether a legend is included. Defaults to `FALSE`
-#' @param gridlines whether to include gridlines. Options are `"both"`, `"none"`, `"x"` (for vertical), and `"y"` (for horizontal). Default is `"both"`
+#' @param gridlines Include gridlines? Options are `"both"`, `"none"`, `"x"` (for vertical), and `"y"` (for horizontal). Default is `"both"`
+#' @param textcolor Color for text elements in `theme()` aesthetics. A named color from the Epinion color palette (see \link[repinion]{getcols_epi}). Include only the color name, not the `"Epinion"` prefix. Default is "Black"
 #' @return a CVI compliant plot
 #' @examples
 #' library(tidyverse)
@@ -13,37 +14,45 @@
 #' @export
 
 epitheme_dust <- function(legend = TRUE,
-                          gridlines = "both") {
+                          gridlines = "both",
+                          textcolor = "Black") {
 
   p <-
     ggplot2::theme_minimal() +
-    ggplot2::theme(axis.text.x = element_text(colour = "black",
+    ggplot2::theme(axis.text.x = element_text(colour = getElement(getcols_epi(), paste0("Epinion ",
+                                                                                        textcolor)),
                                               face = "plain",
                                               size = 12),
-                   axis.text.y = element_text(colour = "black",
+                   axis.text.y = element_text(colour = getElement(getcols_epi(), paste0("Epinion ",
+                                                                                        textcolor)),
                                               face = "plain",
                                               size = 12),
-                   axis.title.x = element_text(colour = "black",
+                   axis.title.x = element_text(colour = getElement(getcols_epi(), paste0("Epinion ",
+                                                                                         textcolor)),
                                                face = "bold",
                                                size = 14,
                                                #vjust = -2.5,
                                                hjust = 1,
                                                margin = ggplot2::margin(t = 20, r = 0, b = 0, l = 0)),
-                   axis.title.y = element_text(colour = "black",
+                   axis.title.y = element_text(colour = getElement(getcols_epi(), paste0("Epinion ",
+                                                                                         textcolor)),
                                                face = "bold",
                                                size = 14,
                                                #vjust = 2.5,
                                                hjust = 1,
                                                margin = ggplot2::margin(t = 0, r = 20, b = 0, l = 0)),
-                   strip.text = element_text(colour = "black",
+                   strip.text = element_text(colour = getElement(getcols_epi(), paste0("Epinion ",
+                                                                                       textcolor)),
                                              face = "plain",
                                              size = 12),
                    strip.background = element_rect(color = getElement(getcols_epi(), "Epinion WarmSand"),
                                                    fill = scales::alpha(getElement(getcols_epi(), "Epinion WarmSand"), 0.4)),
-                   plot.title = element_text(colour = "black",
+                   plot.title = element_text(colour = getElement(getcols_epi(), paste0("Epinion ",
+                                                                                       textcolor)),
                                              face = "bold",
                                              size = 18),
-                   plot.subtitle = element_text(colour = "black",
+                   plot.subtitle = element_text(colour = getElement(getcols_epi(), paste0("Epinion ",
+                                                                                          textcolor)),
                                                 face = "plain",
                                                 size = 16)
     )
