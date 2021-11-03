29043623168c21d0e73ec7bb7d13284dff1798b0
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-03 14:38:50 +0100

    document data

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' DSB colors
+#'
+#' A named vector with Epinion colors in HEX codes
+#'
+#' @format A `list` with 4 elements
+"epi_cols"
+
+
+#' Discrete Epinion color palettes
+#'
+#' @format A `list` with 1 element
+"epipal_d"
+
+
+#' Continuous Epinion color palettes
+#'
+#' @format A `list` with 6 elements
+"epipal_c"
+
+
+
