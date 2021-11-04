797801a9500867aa2a7379c4a24c83788fc027a5
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-04 16:45:07 +0100

    dust.rd

--- R/epitheme_dust.R
+++ R/epitheme_dust.R
@@ -1,6 +1,6 @@
 #' Apply an Epinion styled theme to your `ggplot2` graphics
 #'
-#' This function applies a theme consistent with the Epinion Corporate Visual Identity (CVI) to your `ggplot2` graphics. Add it to your `ggplot2` syntax for conistent visual outputs.
+#' This function applies a warm theme consistent with the Epinion Corporate Visual Identity (CVI) to your `ggplot2` graphics. Add it to your `ggplot2` syntax for conistent visual outputs.
 #' @param legend a logical value indicating whether a legend is included. Defaults to `TRUE` (with `legend.position` defaulting to `"bottom"`)
 #' @param gridlines Include gridlines? Options are `"both"`, `"none"`, `"x"` (for vertical), and `"y"` (for horizontal). Default is `"both"`
 #' @param textcolor Color for text elements in `theme()` options. Must be either "black" or a named color from the Epinion color palette (see \link[repinion]{getcols_epi}), include only the color name, not the `"Epinion"` prefix. Default is "black"
